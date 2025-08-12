d8ffd69a275db2f4b8572d58091eab99d6421061
MeezanOne <meezan@elred.io>
2025-08-12 14:48:40 +0530

    used mysql and updated the model

--- 11-sequelize/controllers/shop.js
+++ 11-sequelize/controllers/shop.js
@@ -2,34 +2,41 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
+    Product.fetchAll()
+  .then(([rows])=>{
+    console.log("Rows Checkup", rows)
+        res.render('shop/product-list', {
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  })
+  .catch(err=> console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.findById(prodId).then(([product])=>{
+    console.log(product)
+     res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+  }).catch(err=>console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll()
+  .then(([rows])=>{
+    console.log("Rows Checkup", rows)
+      res.render('shop/index', {
+        prods: rows,
+        path: '/',
+        pageTitle: 'Shop'
+      });
+  })
+  .catch(err=> console.log(err));
 };
 
 exports.qtyIncrease= (req, res, next) => {
